dc5274337842a9a5d67d827c9a166295cae7f75d
Rodrigo Rocha Gomes e Souza <rodrigorgs@gmail.com>
2017-08-30 14:13:10 -0300

    Adiciona script para compilar todos os jogos.

---
+++ src/compila-tudo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+cd cai-tudo; ../compila.sh JogoPalavras.cc; cd ..
+cd flappy-bird; ../compila.sh Main.cpp; cd ..
+cd ilp-tanks; ../compila.sh main.c; cd ..
+cd kill-the-zombie; ../compila.sh game2.cc; cd ..
+cd memes-ilp; ../compila.sh main.cc; cd ..
+cd pokemon-stay; ../compila.sh ash_catcher.cpp; cd ..
+cd snake; ../compila.sh main.cpp; cd ..
+cd ta-chovendo-hamburguer; ../compila.sh jogo-codigo.cc; cd ..
